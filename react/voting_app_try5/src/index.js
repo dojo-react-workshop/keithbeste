7a4678ac7c99ef0626a45fe7e242c9d43a06b481
Keith <keithbeste@northwwesternmutual.com>
2017-06-02 09:39:29 -0500

    first commit

---
+++ react/voting_app_try5/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+// console.log ('hey');
+
+const App = () => {
+	return <div></div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+// registerServiceWorker();
+	
